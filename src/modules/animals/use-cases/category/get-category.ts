c05dcb8d51de3047940c891d44e1f469786ca879
alonsox <alonsox@users.noreply.github.com>
2021-06-25 15:19:02 -0400

    GetCategory returns  errors as 'CategoryNotFound'

--- src/modules/animals/use-cases/category/get-category.ts
+++ src/modules/animals/use-cases/category/get-category.ts
@@ -1,18 +1,21 @@
 import { categoryDocumentToDto, CategoryDto } from '../../dto/category-dto';
 import { GetCategoryDto } from '../../dto/get-category-dto';
-import { Category, CategoryDocument } from '../../models/category';
+import { Category } from '../../models/category';
 import { CategoryNotFoundError } from './errors/category-not-found';
 
 export async function getCategory(
   categoryInfo: GetCategoryDto,
 ): Promise<CategoryNotFoundError | CategoryDto> {
-  const document: CategoryDocument | null = await Category.findById(
-    categoryInfo.id,
-  ).exec();
+  const { id: categoryId } = categoryInfo;
+  try {
+    const document = await Category.findById(categoryId);
 
-  if (!document) {
-    return new CategoryNotFoundError(categoryInfo.id);
+    if (!document) {
+      return new CategoryNotFoundError(categoryInfo.id);
+    }
+
+    return categoryDocumentToDto(document);
+  } catch (err: any) {
+    return new CategoryNotFoundError(categoryId);
   }
-
-  return categoryDocumentToDto(document);
 }
