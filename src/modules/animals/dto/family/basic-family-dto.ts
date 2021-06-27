03170364beaec0e7414c341c672bfe57fb924030
alonsox <alonsox@users.noreply.github.com>
2021-06-27 12:04:26 -0400

    Added a basic family DTO

---
+++ src/modules/animals/dto/family/basic-family-dto.ts
@@ -0,0 +1,28 @@
+import { LeanDocument } from 'mongoose';
+import { ClassDocument } from '../../models/class';
+
+interface BasicFamilyDtoProps {
+  id: string;
+  name: string;
+}
+
+export class BasicFamilyDto {
+  constructor(private props: BasicFamilyDtoProps) {}
+
+  get id() {
+    return this.props.id;
+  }
+
+  get name() {
+    return this.props.name;
+  }
+}
+
+export function toBasicFamilyDto(
+  familyDoc: LeanDocument<ClassDocument>,
+): BasicFamilyDto {
+  return new BasicFamilyDto({
+    id: familyDoc._id,
+    name: familyDoc.name,
+  });
+}
